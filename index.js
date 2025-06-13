fa6972403df8afe75571e5b555dcb1b215750ef4
dinithii <dinithiwr@gmail.com>
2025-06-13 11:06:33 +0530

    file read and write

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hey I am Node.js");
+const fs = require('fs');
+fs.readFile('movie.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+
+//will overide the already exisitng texts in the .txt file
+fs.writeFile('movie.txt', '!!file wriiten by DInithi!!', function(err) {
+    if(err) throw err;
+    console.log('File saved!');
+});
+
+//will add the new texts to the exising ones
+/*fs.appendFile('movie.txt', '!!file wriiten by DInithi!!', function(err) {
+    if(err) throw err;
+    console.log('File saved!');
+});*/
